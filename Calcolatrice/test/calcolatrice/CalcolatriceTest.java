15c50c24ccca7ec7bec70814b8f6eabeda91c980
Alessandro Accarino <pataponale@gmail.com>
2023-10-26 16:59:01 +0200

    JUnit Test 4.13.2 added

---
+++ Calcolatrice/test/calcolatrice/CalcolatriceTest.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package calcolatrice;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author patap
+ */
+public class CalcolatriceTest {
+    
+    public CalcolatriceTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of main method, of class Calcolatrice.
+     */
+    @Test
+    public void testMain() {
+    }
+    
+}
