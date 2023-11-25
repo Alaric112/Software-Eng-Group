e4b262aab27d4a7185e37923b6a2862f8d3bf173
soniabruno <148860275+soniabruno@users.noreply.github.com>
2023-11-25 17:33:30 +0100

    added test ruleset

---
+++ LiteIFTTT/src/test/java/group2/RulesetTest.java
@@ -0,0 +1,132 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package group2;
+
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RulesetTest {
+
+    private Ruleset ruleset;
+    // Classe di test per l'interfaccia Rule
+    
+    private static class TestRule implements Rule {
+        private String name;
+
+        public TestRule(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void checkRule() {
+            // Implementazione del test
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public Trigger getTrigger() {
+            return null; // Implementazione fittizia per il test
+        }
+
+        @Override
+        public Action getAction() {
+            return null; // Implementazione fittizia per il test
+        }
+    }
+    
+    @Before
+    public void setUp() {
+        // Inizializza un nuovo Ruleset prima di ogni test
+        ruleset = new Ruleset(10, "TestRuleset");
+    }
+
+    @Test
+    public void testAddRule() {
+        // Verifica che la regola sia aggiunta correttamente al Ruleset
+        Rule rule = new TestRule("TestRule");
+        ruleset.addRule(rule);
+
+        ObservableList<Rule> rules = ruleset.getRules();
+        assertTrue(rules.contains(rule));
+    }
+
+    @Test
+    public void testRemoveRule() {
+        // Verifica che la regola sia rimossa correttamente dal Ruleset
+        Rule rule = new TestRule("TestRule");
+        ruleset.addRule(rule);
+
+        ruleset.removeRule(0);
+
+        ObservableList<Rule> rules = ruleset.getRules();
+        assertFalse(rules.contains(rule));
+    }
+
+    @Test
+    public void testSizeRuleSet() {
+        // Verifica che la dimensione del Ruleset sia calcolata correttamente
+        assertEquals(0, ruleset.sizeRuleSet());
+
+        Rule rule1 = new TestRule("TestRule1");
+        Rule rule2 = new TestRule("TestRule2");
+
+        ruleset.addRule(rule1);
+        assertEquals(1, ruleset.sizeRuleSet());
+
+        ruleset.addRule(rule2);
+        assertEquals(2, ruleset.sizeRuleSet());
+
+        ruleset.removeRule(0);
+        assertEquals(1, ruleset.sizeRuleSet());
+    }
+
+    @Test
+    public void testClearRuleSet() {
+        // Verifica che il Ruleset venga cancellato correttamente
+        Rule rule1 = new TestRule("TestRule1");
+        Rule rule2 = new TestRule("TestRule2");
+
+        ruleset.addRule(rule1);
+        ruleset.addRule(rule2);
+
+        ruleset.clearRuleSet();
+
+        ObservableList<Rule> rules = ruleset.getRules();
+        assertTrue(rules.isEmpty());
+    }
+
+    @Test
+    public void testGetName() {
+        // Verifica che il nome del Ruleset sia restituito correttamente
+        assertEquals("TestRuleset", ruleset.getName());
+    }
+
+    @Test
+    public void testSetName() {
+        // Verifica che il nome del Ruleset sia impostato correttamente
+        ruleset.setName("NewTestRuleset");
+        assertEquals("NewTestRuleset", ruleset.getName());
+    }
+
+    @Test
+    public void testGetTimer() {
+        // Verifica che il timer del Ruleset sia restituito correttamente
+        assertEquals(10, ruleset.getTimer());
+    }
+
+    @Test
+    public void testSetTimer() {
+        // Verifica che il timer del Ruleset sia impostato correttamente
+        ruleset.setTimer(20);
+        assertEquals(20, ruleset.getTimer());
+    }
+}
