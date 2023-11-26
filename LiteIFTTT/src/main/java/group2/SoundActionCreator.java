5f87a4c635ae2c1a4340035f470d9f85aad677aa
Alessandro Accarino <pataponale@gmail.com>
2023-11-26 21:01:45 +0100

    Added Test
    - Added MessageAction Test

--- LiteIFTTT/src/main/java/group2/SoundActionCreator.java
+++ LiteIFTTT/src/main/java/group2/SoundActionCreator.java
@@ -14,6 +14,5 @@ public class SoundActionCreator extends ActionCreator {
     public Action createAction() {
         return new SoundAction();
     }
-    
-    
+      
 }
---
+++ LiteIFTTT/src/test/java/group2/MessageActionTest.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package group2;
+
+import javafx.scene.control.Alert;
+import javafx.application.Platform;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author patap
+ */
+public class MessageActionTest {
+    
+    @Test
+    public void testExecute() {
+        // Set up
+        MessageAction messageAction = new MessageAction();
+        String expectedMessage = "Hello world!";
+
+    }
+
+    @Test
+    public void testGetMessageInfo() {
+        MessageAction messageAction = new MessageAction();
+
+        // Use the getter and assert the default value
+        assertEquals("Hello world!", messageAction.getMessageInfo());
+    }   
+    
+    @Test
+    public void testSetMessageInfo() {
+        MessageAction messageAction = new MessageAction();
+
+        // Use the setter to set a new message
+        messageAction.setMessageInfo("New message");
+
+        // Use the getter and assert the new value
+        assertEquals("New message", messageAction.getMessageInfo());
+    }
+    
+}
