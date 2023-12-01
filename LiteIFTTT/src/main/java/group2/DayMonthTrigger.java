b124ec33133bdfc5c3c18a84d177a53bc2829438
lorebor00 <lorebor00@gmail.com>
2023-12-02 00:02:36 +0100

    day of month trigger
    Implemention of a trigger based on the current day-of-month

---
+++ LiteIFTTT/src/main/java/group2/DayMonthTrigger.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package group2;
+
+/**
+ *
+ * @author 39334
+ */
+import java.time.LocalDateTime;
+import java.time.MonthDay;
+
+public class DayMonthTrigger implements Trigger {
+
+    private MonthDay targetMonthDay;
+
+    public DayMonthTrigger(int month, int dayOfMonth) {
+        this.targetMonthDay = MonthDay.of(month, dayOfMonth);
+    }
+
+    @Override
+    public boolean evaluate() {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        MonthDay currentMonthDay = MonthDay.from(currentDateTime);
+
+        return currentMonthDay.equals(targetMonthDay);
+    }
+
+    public MonthDay getTargetMonthDay() {
+        return targetMonthDay;
+    }
+
+    public void setTargetMonthDay(int month, int dayOfMonth) {
+        this.targetMonthDay = MonthDay.of(month, dayOfMonth);
+    }
+}
+
---
+++ LiteIFTTT/src/test/java/group2/DayMonthTriggerTest.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package group2;
+
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.MonthDay;
+
+import static org.junit.Assert.*;
+
+public class DayMonthTriggerTest {
+
+    @Test
+    public void testConstructorAndGetTargetMonthDay() {
+        int month = 5;
+        int dayOfMonth = 15;
+        MonthDay targetMonthDay = MonthDay.of(month, dayOfMonth);
+
+        DayMonthTrigger trigger = new DayMonthTrigger(month, dayOfMonth);
+
+        assertEquals(targetMonthDay, trigger.getTargetMonthDay());
+    }
+
+    @Test
+    public void testSetTargetMonthDay() {
+        int initialMonth = 7;
+        int initialDayOfMonth = 25;
+        int newMonth = 12;
+        int newDayOfMonth = 10;
+
+        DayMonthTrigger trigger = new DayMonthTrigger(initialMonth, initialDayOfMonth);
+        trigger.setTargetMonthDay(newMonth, newDayOfMonth);
+
+        MonthDay expectedTargetMonthDay = MonthDay.of(newMonth, newDayOfMonth);
+
+        assertEquals(expectedTargetMonthDay, trigger.getTargetMonthDay());
+    }
+
+    @Test
+    public void testEvaluateWithNonMatchingMonthDay() {
+        // Assuming the current date is set to May 15
+        int currentMonth = 5;
+        int currentDayOfMonth = 20;
+        MonthDay currentMonthDay = MonthDay.of(currentMonth, currentDayOfMonth);
+
+        // Setting the target MonthDay to a different day
+        int targetMonth = 5;
+        int targetDayOfMonth = 10;
+        DayMonthTrigger trigger = new DayMonthTrigger(targetMonth, targetDayOfMonth);
+
+        assertFalse(trigger.evaluate());
+    }
+
+    // Add more tests based on specific scenarios or edge cases
+}
