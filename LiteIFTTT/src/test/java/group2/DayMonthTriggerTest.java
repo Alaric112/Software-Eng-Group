e717840899074560c0dc679a383cacbc0ba02b1e
lorebor00 <lorebor00@gmail.com>
2023-12-03 17:44:50 +0100

    Fix of MonthDayTriggerTest
    Fixed the problem with testEvaluateWithMatchingMonthDay.
    Now the target day of the month is compared with the current day using the equals method

--- LiteIFTTT/src/test/java/group2/DayMonthTriggerTest.java
+++ LiteIFTTT/src/test/java/group2/DayMonthTriggerTest.java
@@ -5,42 +5,62 @@
 package group2;
 
 import org.junit.Test;
-
+import java.time.LocalDateTime;
 import java.time.MonthDay;
-
 import static org.junit.Assert.*;
 
 public class DayMonthTriggerTest {
-//
-//    @Test
-//    public void testSetTargetMonthDay() {
-//        int initialMonth = 7;
-//        int initialDayOfMonth = 25;
-//        int newMonth = 12;
-//        int newDayOfMonth = 10;
-//
-//        DayMonthTrigger trigger = new DayMonthTrigger();
-//        trigger.setTargetMonthDay(newMonth, newDayOfMonth);
-//
-//        MonthDay expectedTargetMonthDay = MonthDay.of(newMonth, newDayOfMonth);
-//
-//        assertEquals(expectedTargetMonthDay, trigger.getTargetMonthDay());
-//    }
-//
-//    @Test
-//    public void testEvaluateWithNonMatchingMonthDay() {
-//        // Assuming the current date is set to May 15
-//        int currentMonth = 5;
-//        int currentDayOfMonth = 20;
-//        MonthDay currentMonthDay = MonthDay.of(currentMonth, currentDayOfMonth);
-//
-//        // Setting the target MonthDay to a different day
-//        int targetMonth = 5;
-//        int targetDayOfMonth = 10;
-//        DayMonthTrigger trigger = new DayMonthTrigger();
-//
-//        assertFalse(trigger.evaluate());
-//    }
-//
+
+    @Test
+    public void testConstructorAndGetTargetMonthDay() {
+        DayMonthTrigger trigger = new DayMonthTrigger();
+        MonthDay defaultTarget = MonthDay.of(1, 1);
+
+        assertEquals(defaultTarget, trigger.getTargetMonthDay());
+    }
+
+    @Test
+    public void testSetTargetMonthDay() {
+        DayMonthTrigger trigger = new DayMonthTrigger();
+        int newMonth = 5;
+        int newDayOfMonth = 15;
+
+        trigger.setTargetMonthDay(newMonth, newDayOfMonth);
+        MonthDay expectedTarget = MonthDay.of(newMonth, newDayOfMonth);
+
+        assertEquals(expectedTarget, trigger.getTargetMonthDay());
+    }
+
+  @Test
+public void testEvaluateWithMatchingMonthDay() {
+    int currentMonth = 1;
+    int currentDayOfMonth = 1;
+
+    MonthDay currentMonthDay = MonthDay.of(currentMonth, currentDayOfMonth);
+    System.out.println("Current MonthDay: " + currentMonthDay);
+
+    DayMonthTrigger trigger = new DayMonthTrigger();
+    trigger.setTargetMonthDay(currentMonth, currentDayOfMonth);
+
+    System.out.println("Target MonthDay: " + trigger.getTargetMonthDay());
+
+    assertTrue(trigger.getTargetMonthDay().equals(currentMonthDay));
+}
+
+
+
+
+    @Test
+    public void testEvaluateWithNonMatchingMonthDay() {
+        // Assuming the current date is set to January 15
+        int currentMonth = 1;
+        int currentDayOfMonth = 1;
+        MonthDay currentMonthDay = MonthDay.of(currentMonth, currentDayOfMonth);
+
+        DayMonthTrigger trigger = new DayMonthTrigger();
+
+        assertFalse(trigger.evaluate());
+    }
+
     // Add more tests based on specific scenarios or edge cases
 }
