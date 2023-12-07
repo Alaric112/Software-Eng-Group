908bfad7a765829fb30cec4f7a0616787ded7bbc
lorebor00 <lorebor00@gmail.com>
2023-12-07 16:40:02 +0100

    Implementation of ExitStatusTrigger
    Trigger that checks the exit status of a specified external program. It checks if the exit status of a specified external program is equal to the exit status set by the user

--- LiteIFTTT/src/main/java/group2/Model/Trigger/ExitStatusTrigger.java
+++ LiteIFTTT/src/main/java/group2/Model/Trigger/ExitStatusTrigger.java
@@ -6,8 +6,54 @@ package group2.Model.Trigger;
 
 /**
  *
- * @author 39334
+ * @author Lore
  */
-public class ExitStatusTrigger {
+import java.io.IOException;
+
+public class ExitStatusTrigger implements Trigger {
+
+    private String externalProgram;
+    private int userExitStatus;
+
+    public ExitStatusTrigger(String externalProgram, int userExitStatus) {
+        this.externalProgram = externalProgram;
+        this.userExitStatus = userExitStatus;
+    }
+
+    @Override
+    public boolean evaluate() {
+        try {
+            // Configure the process
+            ProcessBuilder processBuilder = new ProcessBuilder(externalProgram);
+            Process process = processBuilder.start();
+
+            //get the exit status
+            int exitStatus = process.waitFor();
+            
+            // Check if the exit status matches the expected exit status
+            return exitStatus == userExitStatus;
+
+        } catch (IOException | InterruptedException e) {
+            // Handle exceptions if the process configuration fails
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // Getter and setter methods for externalProgram
+    public String getExternalProgram() {
+        return externalProgram;
+    }
+
+    public void setExternalProgram(String externalProgram) {
+        this.externalProgram = externalProgram;
+    }
     
+    // Getter and Setter methods for UserExitStatus
+    public int getUserExitStatus() {
+        return userExitStatus;
+    }
+    public void setUserExitStatus(int userExitStatus) {
+        this.userExitStatus = userExitStatus;
+    }
 }
