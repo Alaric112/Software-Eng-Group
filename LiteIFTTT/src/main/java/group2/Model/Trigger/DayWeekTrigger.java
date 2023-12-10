4ab5221ab0ee7fdb15595c98a2e6134678f3c778
lorebor00 <lorebor00@gmail.com>
2023-12-10 19:03:43 +0100

    javadoc DayWeekTrigger

--- LiteIFTTT/src/main/java/group2/Model/Trigger/DayWeekTrigger.java
+++ LiteIFTTT/src/main/java/group2/Model/Trigger/DayWeekTrigger.java
@@ -8,15 +8,19 @@ import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 
 /**
- * The `DayWeekTrigger` class represents an implementation of the `Trigger` interface
+ * The DayWeekTrigger class represents an implementation of the Trigger interface
  * for triggering events based on a specific day of the week.
  *
  * @author Lore
  */
 public class DayWeekTrigger implements Trigger {
 
+    /** The target day of the week for triggering an event. */
     private DayOfWeek targetDay;
 
+    /**
+     * Constructs a new DayWeekTrigger with the default target day set to Monday.
+     */
     public DayWeekTrigger() {
         this.targetDay = DayOfWeek.MONDAY;
     }
@@ -24,8 +28,8 @@ public class DayWeekTrigger implements Trigger {
     /**
      * Evaluates whether the current day matches the target day for triggering an event.
      *
-     * @return `true` if the current day matches the target day; otherwise, `false`.
-     */    
+     * returns true if the current day matches the target day, otherwise false.
+     */
     @Override
     public boolean evaluate() {
         LocalDateTime currentDateTime = LocalDateTime.now();
@@ -34,17 +38,28 @@ public class DayWeekTrigger implements Trigger {
         return currentDay == targetDay;
     }
 
+    /**
+     * Gets the target day of the week.
+     *
+     */
     public DayOfWeek getTargetDay() {
         return targetDay;
     }
 
+    /**
+     * Sets the target day of the week.
+     *
+     */
     public void setTargetDay(DayOfWeek targetDay) {
         this.targetDay = targetDay;
     }
 
+    /**
+     * Returns a string representation of the DayWeekTrigger object.
+     *
+     */
     @Override
     public String toString() {
-        return "Day of Week selected: "+ targetDay;
+        return "Day of Week selected: " + targetDay;
     }
-    
 }
